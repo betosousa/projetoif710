87c2a1ba8ad2332901d061e6a537111b427c3ce2
betosousa <avss@cin.ufpe.br>
2017-09-30 11:09:44 -0300

    item 5

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
@@ -2,16 +2,30 @@ package br.ufpe.cin.if710.podcast.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import br.ufpe.cin.if710.podcast.R;
+import br.ufpe.cin.if710.podcast.domain.ItemFeed;
 
 public class EpisodeDetailActivity extends Activity {
+
+    public static final String ITEM_FEED = "itemFeed";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_detail);
 
-        //TODO preencher com informações do episódio clicado na lista...
+
+        ItemFeed itemFeed = (ItemFeed) getIntent().getSerializableExtra(ITEM_FEED);
+
+        TextView title = (TextView) findViewById(R.id.podcastitle);
+        title.setText(itemFeed.getTitle());
+
+        TextView pubDate = (TextView) findViewById(R.id.pubDate);
+        pubDate.setText(itemFeed.getPubDate());
+
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(itemFeed.getDescription());
     }
 }
