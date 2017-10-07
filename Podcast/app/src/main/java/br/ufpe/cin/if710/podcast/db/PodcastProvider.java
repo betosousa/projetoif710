1f8b08a5a6b20d336b9c1141a68fcd03baa8c042
betosousa <avss@cin.ufpe.br>
2017-10-06 21:37:15 -0300

    ajustes no insert do provider

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/db/PodcastProvider.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/db/PodcastProvider.java
@@ -4,6 +4,7 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 
 public class PodcastProvider extends ContentProvider {
 
@@ -34,7 +35,27 @@ public class PodcastProvider extends ContentProvider {
         Uri contentUri = null;
         // se a uri eh valida, insere no BD recuperando o id
         if(isEpisodeTableUri(uri)){
-            long id = dbHelper.getWritableDatabase().insert(PodcastDBHelper.DATABASE_TABLE, null, values);
+            long id;
+            // verifica se item ja existe no BD
+            Cursor c = dbHelper.getReadableDatabase().query(PodcastDBHelper.DATABASE_TABLE,
+                    PodcastDBHelper.columns,
+                    PodcastDBHelper.EPISODE_TITLE + "=?",
+                    new String[]{(String) values.get(PodcastProviderContract.TITLE)},
+                    null, null, null);
+
+            if (c != null) {
+                c.moveToFirst();
+                try {
+                    // caso exista, recupera o id
+                    id = c.getLong(c.getColumnIndex(PodcastDBHelper._ID));
+                } catch (IndexOutOfBoundsException e) {
+                    // caso contrario, insere
+                    id = dbHelper.getWritableDatabase().insert(PodcastDBHelper.DATABASE_TABLE, null, values);
+                }
+            } else {
+                // caso contrario, insere
+                id = dbHelper.getWritableDatabase().insert(PodcastDBHelper.DATABASE_TABLE, null, values);
+            }
             contentUri = Uri.withAppendedPath(PodcastProviderContract.EPISODE_LIST_URI, Long.toString(id));
         }
         // retorna a URI de acesso ao item criado
@@ -72,7 +93,7 @@ public class PodcastProvider extends ContentProvider {
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
         int updatedRows = 0;
-        // se a uri eh valida, realiza a query recuperando a qtd de linhas deletadas
+        // se a uri eh valida, realiza a query recuperando a qtd de linhas atualizadas
         if (isEpisodeTableUri(uri)) {
             updatedRows = dbHelper.getWritableDatabase().update(PodcastDBHelper.DATABASE_TABLE, values, selection, selectionArgs);
         }
